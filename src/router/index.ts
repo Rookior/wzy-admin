c855c4b3b6d2839ae54511c56beaff1c8f7f994f
weizy <weizy@bjyn.com>
2023-10-18 17:06:49 +0800

    增加异常页面404、403、500

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,6 +20,24 @@ const router = createRouter({
           component: () => import('../views/AboutView.vue'),
           name: 'About',
           meta: { title: 'About', icon: 'about' }
+        },
+        {
+          path: '404',
+          component: () => import('../views/errorPage/404.vue'),
+          name:'404',
+          meta: { title: '404', icon: '404' }
+        },
+        {
+          path: '403',
+          component: () => import('../views/errorPage/403.vue'),
+          name:'403',
+          meta: { title: '403', icon: '403' }
+        },
+        {
+          path: '500',
+          component: () => import('../views/errorPage/500.vue'),
+          name:'500',
+          meta: { title: '500', icon: '500' },
         }
       ]
     },
@@ -37,6 +55,17 @@ const router = createRouter({
       ]
     }
   ]
+})
+
+// 路由守卫
+router.beforeEach((to, from,next) => {
+  console.log(to,from)
+  if (to.matched.length === 0) {
+    console.log(to, '跳转已被拦截')
+    next({ path: '/404' })
+    return false
+  }
+  next()
 })
 
 export default router
